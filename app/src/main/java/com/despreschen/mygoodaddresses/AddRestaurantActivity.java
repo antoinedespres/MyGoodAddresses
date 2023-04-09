d16690c25246abe4f9e2a8a8fdccd2327c7faba9
Antoine Després <58844676+antoinedespres@users.noreply.github.com>
2023-04-09 10:53:28 +0200

    feat: add the screen for creating a restaurant
    Also replaces the generated launcher icon with a plate.
    
    Co-authored-by: Xing Chen <60323726+xingcdev@users.noreply.github.com>

---
+++ app/src/main/java/com/despreschen/mygoodaddresses/AddRestaurantActivity.java
@@ -0,0 +1,13 @@
+package com.despreschen.mygoodaddresses;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddRestaurantActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_restaurant);
+    }
+}
--- app/src/main/java/com/despreschen/mygoodaddresses/MainActivity.java
+++ app/src/main/java/com/despreschen/mygoodaddresses/MainActivity.java
@@ -2,7 +2,10 @@ package com.despreschen.mygoodaddresses;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent i = new Intent(this, AddRestaurantActivity.class);
+        startActivity(i);
+        return super.onOptionsItemSelected(item);
     }
 }
